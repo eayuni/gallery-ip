c9bfa4954ab6ede5aaa2c665cf2bc1c7cf9c3bd5
Eunice Otieno <otieunice@gmail.com>
2022-03-29 11:41:46 +0300

    updated config

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://eunice123:eunice123@galleryapp.rcmum.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    development: 'mongodb+srv://eunice123:eunice123@galleryapp.rcmum.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    production: 'mongodb+srv://eunice123:<>@galleryapp.rcmum.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    development: 'mongodb+srv://eunice123:<>@galleryapp.rcmum.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
    // test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
     test:'mongodb+srv://eunice123:eunice123@galleryapp.rcmum.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
